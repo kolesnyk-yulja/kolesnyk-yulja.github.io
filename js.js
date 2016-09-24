943305ba5dadc1303b6069449799dbb3845273c9
Kolesnyk Yulja <kolesnyk_yulja@ukr.net>
2016-09-24 12:52:06 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,28 @@
+  $('#slider_header').slick({
+  	dots: true
+  	});
+
+    $('.project_sl').slick({
+  	slidesToShow: 4,
+  slidesToScroll: 1
+  });
+
+      $('.team_sl').slick({
+  	slidesToShow: 4,
+  slidesToScroll: 3,
+  dots: true
+  });
+
+
+  $('.news_sl').slick({
+  	slidesToShow: 2,
+  slidesToScroll: 2,
+  dots: true
+  });
+
+
+      $('.clients_sl').slick({
+  	slidesToShow: 8,
+  slidesToScroll: 1
+
+  });
